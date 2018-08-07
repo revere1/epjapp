c92a979c1be76244e9ca9f5c713d0c7a878b498d
Anusha M <anusha@anagha.local>
2018-08-07 11:37:49 +0530

    commited for code

---
+++ src/Global.ts
@@ -0,0 +1,11 @@
+
+const serverURI =  'http://192.168.2.6:1332/app/' 
+const apiURI  =  'http://192.168.2.6:1332/' 
+
+export const Global = {
+  SERVER_URL:serverURI,
+  api_code : '6209232b45aea869fb846a89d263fb3a',
+  User : null,
+  BASE_API: apiURI,
+  productId:null
+};
